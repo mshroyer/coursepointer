e7ddedfda942b6e3c543fa7314fc7e90e59d349c
Mark Shroyer <mark@shroyer.name>
2025-04-09 11:55:12 -0700

    [main] Print messages from FIT files

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import argparse
+
+import garmin_fit_sdk as fit
+
+
 def main():
-    print("Hello from coursepointer!")
+    parser = argparse.ArgumentParser(description="Examine course points in a FIT course")
+    parser.add_argument("path")
+    args = parser.parse_args()
+
+    stream = fit.Stream.from_file(args.path)
+    decoder = fit.Decoder(stream)
+    messages, errors = decoder.read()
+
+    print("Errors: {}".format(errors))
+    print(messages.keys())
+    print(messages["course_point_mesgs"])
 
 
 if __name__ == "__main__":
