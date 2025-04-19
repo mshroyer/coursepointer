d33ad33daff86f883e7a41e77321277b8cab4529
Mark Shroyer <mark@shroyer.name>
2025-04-19 11:24:38 -0700

    [dotnet] Add Tests package with Expecto
    Used `dotnet new packageprops` first to enable central package management.

---
+++ dotnet/tests/CoursePointer.Tests/Main.fs
@@ -0,0 +1,11 @@
+﻿open Expecto
+
+let tests =
+    test "An example test" {
+        let subject = "Hello world"
+        Expect.equal "Hello world" subject "The strings should be equal"
+    }
+
+[<EntryPoint>]
+let main args =
+    runTestsWithCLIArgs [] args tests
