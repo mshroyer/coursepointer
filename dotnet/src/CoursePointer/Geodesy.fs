598668ac438212fc6cd1eb7eb38b1d08bf559bd9
Mark Shroyer <mark@shroyer.name>
2025-04-24 13:33:55 -0700

    [dotnet] Add simple test of computing distance

--- dotnet/src/CoursePointer/Geodesy.fs
+++ dotnet/src/CoursePointer/Geodesy.fs
@@ -2,12 +2,24 @@ module CoursePointer.Geodesy
 
 open FSharp.Data.UnitSystems.SI.UnitSymbols
 
-[<Measure>] type deg
+open GeographicLib
+
+[<Measure>]
+type deg
 
 [<Struct>]
-type SurfacePoint =
-    { Lat: double<deg>
-      Lon: double<deg> }
+type SurfacePoint = { Lat: double<deg>; Lon: double<deg> }
+
+[<Struct>]
+type InverseResult = { Length: double<m>; Azimuth1: double<deg>; Azimuth2: double<deg> }
 
 let getDistance a b : double<m> =
-    0.0<m>
+    let result =
+        Geodesic.WGS84.Inverse(
+            double a.Lat,
+            double a.Lon,
+            double b.Lat,
+            double b.Lon,
+            GeodesicFlags.Distance ||| GeodesicFlags.Azimuth
+        )
+    result.Distance * 1.0<m>
--- dotnet/tests/CoursePointer.Tests/Main.fs
+++ dotnet/tests/CoursePointer.Tests/Main.fs
@@ -1,4 +1,8 @@
-﻿open Expecto
+﻿open FSharp.Data.UnitSystems.SI.UnitSymbols
+
+open Expecto
+
+open CoursePointer.Geodesy
 
 [<Tests>]
 let tests =
@@ -8,6 +12,15 @@ let tests =
         
         let sum = CoursePointer.Say.addSome 2 3
         Expect.equal sum 5 "2+3 should equal 5"
+    }
+    
+[<Tests>]
+let geodesyTests =
+    test "Test distance calculation" {
+        let p1 = { SurfacePoint.Lat = 0.0<deg>; Lon = 0.0<deg> }
+        let p2 = { SurfacePoint.Lat = 5.0<deg>; Lon = 5.0<deg> }
+        let distance = getDistance p1 p2
+        Expect.isTrue (abs(distance - 784029.0<m>) < 1.0<m>) "Expect distances to be approximately equal"
     }
 
 [<EntryPoint>]
