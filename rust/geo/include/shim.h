39cd266e35d9cf55ed19f4611df00cfdabaaa59d
Mark Shroyer <mark@shroyer.name>
2025-04-24 13:46:17 -0700

    [rust] Add comment to the GetWGS84() shim
    Notes its purpose to work around current limitation in cxx.

--- rust/geo/include/shim.h
+++ rust/geo/include/shim.h
@@ -5,6 +5,13 @@ namespace GeographicLib {
 
 class Geodesic;
 
+/**
+ * Get the global instance of the WGS84 ellipsoid.
+ *
+ * This is a shim to invoke GeographicLib::Geodesic::WGS84(), since at the time
+ * of writing the cxx binding generator seemingly can't invoke static member
+ * functions.
+ */
 const Geodesic& GetWGS84();
 
 }  // namespace GeographicLib
