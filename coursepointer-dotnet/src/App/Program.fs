32a46152abbc8434414ca00946b3ee6327aaf906
Mark Shroyer <mark@shroyer.name>
2025-05-02 22:05:43 -0700

    [dotnet] Add lap to test course file

--- coursepointer-dotnet/src/App/Program.fs
+++ coursepointer-dotnet/src/App/Program.fs
@@ -3,20 +3,41 @@ open System.IO
 
 open Dynastream.Fit
 
-type DisposableEncoder(encoder: Encode) =
+type EncoderWrapper(stream: Stream) =
+    let encoder = new Encode(ProtocolVersion.V10)
+    do encoder.Open(stream)
+    
     member _.Encoder = encoder
     
     interface IDisposable with
         member this.Dispose() = this.Encoder.Close()
-
+        
 let writeTestCourse path =
     use stream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.Read)
-    use wrapper = new DisposableEncoder(new Encode(ProtocolVersion.V10))
+    use wrapper = new EncoderWrapper(stream)
     
-    wrapper |> _.Encoder |> _.Open(stream)
+    wrapper.Encoder |> fun encoder ->
+        let fileId = new FileIdMesg()
+        fileId.SetType(File.Course)
+        fileId.SetManufacturer(Manufacturer.Development)
+        fileId.SetProduct(0x0001us)
+        fileId.SetSerialNumber(0x0001u)
+        encoder.Write(fileId)
+        
+        let course = new CourseMesg()
+        course.SetSport(Sport.Cycling)
+        course.SetName("Test Course")
+        encoder.Write(course)
+        
+        let lap = new LapMesg()
+        lap.SetStartTime(new Dynastream.Fit.DateTime(DateTime.Now))
+        lap.SetTotalElapsedTime(3600.0f)
+        encoder.Write(lap)
+        
+        ()
 
 [<EntryPoint>]
 let main _ =
-    printfn "Hello, world!"
+    printfn "Writing test course"
     writeTestCourse("out.fit")
     0
