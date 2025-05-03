0d14c7584080110bcffb5a86a65c0d4d4eeb036a
Mark Shroyer <mark@shroyer.name>
2025-05-02 20:18:59 -0700

    [dotnet] Write bare FIT file with Garmin SDK

--- coursepointer-dotnet/src/App/Program.fs
+++ coursepointer-dotnet/src/App/Program.fs
@@ -1,4 +1,22 @@
-﻿[<EntryPoint>]
+﻿open System
+open System.IO
+
+open Dynastream.Fit
+
+type DisposableEncoder(encoder: Encode) =
+    member this.Encoder = encoder
+    
+    interface IDisposable with
+        member this.Dispose() = this.Encoder.Close()
+
+let writeTestCourse path =
+    use stream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.Read)
+    use wrapper = new DisposableEncoder(new Encode(ProtocolVersion.V10))
+    
+    wrapper |> _.Encoder |> fun encoder -> encoder.Open(stream)
+
+[<EntryPoint>]
 let main _ =
     printfn "Hello, world!"
+    writeTestCourse("out.fit")
     0
