5ff2a5a071c0803cfea27883063a77bcc08e1a13
Mark Shroyer <mark@shroyer.name>
2025-05-02 20:25:00 -0700

    [dotnet] Use lambda shorthand

--- coursepointer-dotnet/src/App/Program.fs
+++ coursepointer-dotnet/src/App/Program.fs
@@ -4,7 +4,7 @@ open System.IO
 open Dynastream.Fit
 
 type DisposableEncoder(encoder: Encode) =
-    member this.Encoder = encoder
+    member _.Encoder = encoder
     
     interface IDisposable with
         member this.Dispose() = this.Encoder.Close()
@@ -13,7 +13,7 @@ let writeTestCourse path =
     use stream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.Read)
     use wrapper = new DisposableEncoder(new Encode(ProtocolVersion.V10))
     
-    wrapper |> _.Encoder |> fun encoder -> encoder.Open(stream)
+    wrapper |> _.Encoder |> _.Open(stream)
 
 [<EntryPoint>]
 let main _ =
