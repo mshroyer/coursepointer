4923aabdd55222396924c98f56dd9fb49fd02068
Mark Shroyer <mark@shroyer.name>
2025-05-04 16:06:37 -0700

    [dotnet] Doc comments on units of measure

--- coursepointer-dotnet/src/CoursePointer/Measures.fs
+++ coursepointer-dotnet/src/CoursePointer/Measures.fs
@@ -2,21 +2,26 @@ module CoursePointer.Measures
 
 open FSharp.Data.UnitSystems.SI.UnitSymbols
 
+/// Kilometer
 [<Measure>]
 type km
 
+/// Millisecond
 [<Measure>]
 type ms
 
+/// Hour
 [<Measure>]
 type hr
 
-(* Unit conversions *)
-let mPerKm = 1000.0<m/km>
-let sPerHr = 3600.0<s/hr>
-
+/// Degree
 [<Measure>]
 type deg
 
+/// Semicircle
 [<Measure>]
 type semicircle
+
+// Unit conversions
+let mPerKm = 1000.0<m/km>
+let sPerHr = 3600.0<s/hr>
