669f4b39f2346b429ea7698aa505ddcc4c7aa28f
Mark Shroyer <mark@shroyer.name>
2025-05-15 21:06:51 -0700

    [integration] Reorganization integration directory

--- integration/README.md
+++ integration/README.md
@@ -1,3 +1,7 @@
+# Integration tests
+
+This directory contains Python-based integration tests of the Rust CLI.
+
 ## Data files
 
 - cptr001.fit: A simple course created on Garmin Connect on the web, with no course points.
---
+++ integration/src/integration/__init__.py
@@ -0,0 +1,9 @@
+import garmin_fit_sdk
+
+
+def validate_fit_file(path: str) -> None:
+    stream = garmin_fit_sdk.Stream.from_file(path)
+    decoder = garmin_fit_sdk.Decoder(stream)
+    messages, errors = decoder.read()
+    if errors:
+        raise ValueError(f"Errors reading FIT file: {errors}")
