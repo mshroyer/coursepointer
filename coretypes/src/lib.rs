db45bc3471542c8afbf7c5f7f9616b85f56ba568
Mark Shroyer <mark@shroyer.name>
2025-05-25 17:53:19 -0700

    [coretypes] Inline GeoPoint's accessors
    Inspection with godbolt.org local disassembly suggests this may be
    necessary.

--- coretypes/src/lib.rs
+++ coretypes/src/lib.rs
@@ -42,16 +42,19 @@ impl GeoPoint {
     }
     
     /// Get point latitude
+    #[inline(always)]
     pub fn lat(&self) -> Degrees<f64> {
         self.lat
     }
     
     /// Get point longitude
+    #[inline(always)]
     pub fn lon(&self) -> Degrees<f64> {
         self.lon
     }
     
     /// Get point elevation, if known
+    #[inline(always)]
     pub fn ele(&self) -> Option<Meters<f64>> {
         self.ele
     }
