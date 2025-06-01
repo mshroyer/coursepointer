9494160d4b61ebaa4ec06a8184658f2ed91629a8
Mark Shroyer <mark@shroyer.name>
2025-06-01 06:13:05 -0700

    [integration] Merge FIT header test cases
    Slight speed up by avoiding duplicate writes

--- integration/tests/test_fit_encoding.py
+++ integration/tests/test_fit_encoding.py
@@ -42,16 +42,7 @@ from integration import (
 # - file_creator
 
 
-def test_empty_course(tmpdir, integration_stub):
-    spec = CourseSpec()
-    spec.write_file(tmpdir / "spec.json")
-    integration_stub(
-        "write-fit", "--spec", tmpdir / "spec.json", "--out", tmpdir / "out.fit"
-    )
-    garmin_read_messages(tmpdir / "out.fit")
-
-
-def test_header_protocol_version(tmpdir, integration_stub):
+def test_header_fields(tmpdir, integration_stub):
     spec = CourseSpec()
     spec.write_file(tmpdir / "spec.json")
     integration_stub(
@@ -62,19 +53,8 @@ def test_header_protocol_version(tmpdir, integration_stub):
     # Protocol version 1 is represented as 0x10, 2 as 0x20.
     assert header.protocol_version == 0x10
 
-
-def test_header_profile_version(tmpdir, integration_stub):
-    # Get the self-reported Garmin global profile version from the library.
-    lib_profile_version = int(integration_stub("show-profile-version").stdout.strip())
-
-    spec = CourseSpec()
-    spec.write_file(tmpdir / "spec.json")
-    integration_stub(
-        "write-fit", "--spec", tmpdir / "spec.json", "--out", tmpdir / "out.fit"
-    )
-    header = garmin_read_file_header(tmpdir / "out.fit")
-
     # The output file should encode the same profile version.
+    lib_profile_version = int(integration_stub("show-profile-version").stdout.strip())
     assert header.profile_version == lib_profile_version
 
 
