65bc83e71a7fb3ab006b89d331ed433044300122
Mark Shroyer <mark@shroyer.name>
2025-07-25 08:39:05 -0700

    [web] Add some explanation text, work on layout

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,18 +9,25 @@ import init, {
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <header>
-    <p>Header</p>
+    <p><b>CoursePointer</b> — Convert GPX waypoints to FIT course points</p>
   </header>
   <main>
-    <input id="picker" type="file" />
-    <pre id="report"></pre>
-    <div id="download"></div>
+    <div id="explainer">
+      <div>
+        <p>Choose a GPX file containing exactly one route or track. This will generate a corresponding Garmin FIT course 
+        file, in which any waypoints located along the route have been converted to FIT course points.</p>
+        <p>See the <a href="https://github.com/mshroyer/coursepointer/blob/main/README.md">README</a>
+        for more about what this does, and why.</p>
+      </div>
+    </div>
   </main>
   <aside>
-    <p>Sidebar</p>
+    <input id="picker" type="file" />
+    <p>CoursePointer runs in your browser using WebAssembly. It does not upload your course anywhere.</p>
   </aside>
   <footer>
-    <p>Footer</p>
+    <p>© 2025 Mark Shroyer, <a href="https://github.com/mshroyer/coursepointer/blob/main/docs/third_party_licenses.md">MIT licensed</a>.
+     Source code is on <a href="https://github.com/mshroyer/coursepointer">GitHub</a>.</p>
   </footer>
 `;
 
@@ -29,6 +36,11 @@ function setupPicker(p: HTMLInputElement) {
     const target = e.target as HTMLInputElement;
     const file: File | undefined = target.files?.[0];
     if (!file) return;
+
+    document.querySelector<HTMLElement>("main")!.innerHTML = `
+  <pre id="report"></pre>
+  <div id="download"></div>
+`;
 
     const buf = await file.arrayBuffer();
     console.time("read_gpx_bytes");
@@ -41,7 +53,7 @@ function setupPicker(p: HTMLInputElement) {
     });
 
     document.querySelector<HTMLDivElement>("#download")!.innerHTML = `
-  <button id="downloadbtn" type="button">Download</button>
+  <button id="downloadbtn" type="button">Save as FIT</button>
 `;
     document
       .querySelector<HTMLButtonElement>("#downloadbtn")!
