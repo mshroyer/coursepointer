9ee60f4fd8f96e0079d36156508abbeff06807ed
Mark Shroyer <mark@shroyer.name>
2025-07-31 07:15:37 -0700

    [web] Add @vitejs/plugin-basic-ssl
    This causes the Vite dev server to run with a self-signed certificate,
    making Chrome's blocking of insecure downloads less of a pain to deal
    with.

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+    plugins: [
+        basicSsl()
+    ],
     server: {
         host: '0.0.0.0', // Listen on all network interfaces
         port: 5173,       // Optional: default is 5173
